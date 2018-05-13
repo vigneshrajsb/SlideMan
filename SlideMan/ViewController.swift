841d0b135908b46fadfd2d73931a01c5e8501ac2
Vigneshraj Sekar Babu <vigneshrajsekarbabu@vigneshrajs-macbook-pro.local>
2018-05-13 16:34:08 -0500

    Initial Commit

---
+++ SlideMan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMan
+//
+//  Created by Vigneshraj Sekar Babu on 5/13/18.
+//  Copyright © 2018 Vigneshraj Sekar Babu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
